5c1a666a247490dbecf3b8160a05ca0f25046dce
CaraH03 <1304164@rgu.ac.uk>
2016-11-28 21:14:54 +0000

    Merge branch 'master' of https://github.com/zaklinarycko/Coursework into ScarletWitch
    # Conflicts:
    #	index.html

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,12 @@
 <?php
 
-$db=mysqli(
+$db= new mysqli(
     "br-cdbr-azure-south-b.cloudapp.net",
     "bc852cafdb9783",
     "187e9998",
-    "coursework"
+    "acsm_16b13eeb6a004c1"
     );
 
-if($db->connect_errno){
-    die('Connectfailed['.$db->connect_error.']');
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
 }
