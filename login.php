0fe0f3332070a91a512be4fbc37295f9bae72763
CaraH03 <1304164@rgu.ac.uk>
2016-12-02 21:07:32 +0000

    Merge branch 'master' of https://github.com/zaklinarycko/Coursework into ScarletWitch
    # Conflicts:
    #	dbconnect.php

--- login.php
+++ login.php
@@ -9,36 +9,38 @@
 <body>
 
 <?
+//Opens up a session
 session_start();
-
-include ("dbconnect.php");
-
-if (isset($_SESSION['username']))
+include ("db_connect.php");
+//Check to see if the 'username' session variable is set.
+if (isset($_SESSION['username'])) //SESSION DOES EXIST
 {
-    echo "<p>Hello" . $_SESSION['username'] . "</p>";
+    echo "<p>Hello " . $_SESSION['username'] . "</p>";
     $sql = "SELECT * FROM users WHERE username='". $_SESSION['username'] . "'";
     $result = $db->query($sql);
     while($row = $result->fetch_array())
     {
-        echo "<p>User type is " . $row['userType']."</p>";
+        echo "<p>User type is " . $row['userType'] . "</p>";
     }
     ?>
-    <a href=""logout.php">Logout</a>
+    <a href="logout.php">Logout</a>
     <?
 }
 else //SESSION DOESNT EXIST
 {
     ?>
+
     <h1>My Login Form</h1>
     <form method="post" action="checklogin.php">
         <p><input type="text" name="username" value="" placeholder="Username or Email"></p>
         <p><input type="password" name="password" value="" placeholder="Password"></p>
-        <p class=""submit"><input type="submit" name="commit" value="Login"></p>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>
     </form>
+    <a href="signup.php">Signup</a>
+
     <?
 }
 ?>
-
 
 <div id="left" class="column">
     <ul>
