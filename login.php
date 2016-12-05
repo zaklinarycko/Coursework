7b868e7dc9b6bd71a90911407a129dd7b547f1e8
Zaki <1404039@rgu.ac.uk>
2016-12-05 14:47:51 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	login.php
    #	news.html

--- login.php
+++ login.php
@@ -49,8 +49,8 @@
 
             <h1>My Login Form</h1>
             <form method="post" action="checklogin.php">
-                <p><input type="text" name="username" value="" placeholder="Username or Email"></p>
-                <p><input type="password" name="password" value="" placeholder="Password"></p>
+                <p><input type="text" name="Username" value="" placeholder="Username or Email"></p>
+                <p><input type="password" name="Password" value="" placeholder="Password"></p>
                 <p class="submit"><input type="submit" name="commit" value="Login"></p>
             </form>
 
