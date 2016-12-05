0d2c2920fc182dcc51a6921fbfa3d9cc837fcfa5
Zaki <1404039@rgu.ac.uk>
2016-12-05 14:39:27 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	login.php
    #	news.html

--- login.php
+++ login.php
@@ -26,10 +26,6 @@
 
         <?
 
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
-
         //Opens up a session
         session_start();
         include ("dbconnect.php");
