ef8489ffcd7cdc4c9aab64a58611de0ce1ffc343
Zaki <1404039@rgu.ac.uk>
2016-12-05 16:55:06 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	login.php
    #	news.html

--- clubs.php
+++ clubs.php
@@ -3,33 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style/main.css"/>
-
+    <link rel="stylesheet" href="style/clubs.css"/>
 </head>
-
 <body>
+
+
 <div id="left" class="column">
     <ul>
         <img src="header.jpg" alt="Main Image" style="width:200px;height:200px;">
         <li><a href="welcome.php">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="clubs.html">Clubs</a></li>
-        <li><a href="news.html">News</a></li>
-<<<<<<< Updated upstream
-        <li><a href="login.php">Log In!</a></li>
-=======
+        <li><a href="about.php">About</a></li>
+        <li><a href="clubs.php">Clubs</a></li>
+        <li><a href="news.php">News</a></li>
         <li><a href="login.php">Log In</a></li>
->>>>>>> Stashed changes
 
     </ul>
 </div>
+
+
 <div id="container">
     <div id="center" class="column">
-        <img src="header.jpg" alt="Main Image">
+        <input type="text" placeholder="Placeholder text blah blah." />
     </div>
-
-    <div id="footer"> Page designed and created by Zaklina Rycko,Cara Henderson, Sarah Thomson, Sarah Valentine and Lucy Axford</div>
-
 </div>
+
+
+<div id="footer">Page designed and created by Zaklina Rycko,Cara Henderson, Sarah Thomson, Sarah Valentine and Lucy Axford</div>
 
 </body>
 </html>
--- news.php
+++ news.php
@@ -3,32 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style/main.css"/>
-    <link rel="stylesheet" href="style/clubs.css"/>
+
 </head>
+
 <body>
-
-
 <div id="left" class="column">
     <ul>
         <img src="header.jpg" alt="Main Image" style="width:200px;height:200px;">
         <li><a href="welcome.php">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="clubs.html">Clubs</a></li>
-        <li><a href="news.html">News</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="clubs.php">Clubs</a></li>
+        <li><a href="news.php">News</a></li>
         <li><a href="login.php">Log In</a></li>
 
     </ul>
 </div>
-
-
 <div id="container">
     <div id="center" class="column">
-        <input type="text" placeholder="Placeholder text blah blah." />
+        <img src="header.jpg" alt="Main Image">
     </div>
+
+    <div id="footer"> Page designed and created by Zaklina Rycko,Cara Henderson, Sarah Thomson, Sarah Valentine and Lucy Axford</div>
+
 </div>
-
-
-<div id="footer">Page designed and created by Zaklina Rycko,Cara Henderson, Sarah Thomson, Sarah Valentine and Lucy Axford</div>
 
 </body>
 </html>
