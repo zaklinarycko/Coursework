69972d585a4e8bed07e65ced2c356ddb10490ea2
Zaki <1404039@rgu.ac.uk>
2016-12-07 19:09:10 +0000

    Merge branch 'master' of https://github.com/zaklinarycko/Coursework into Blackwidow
    # Conflicts:
    #	about.php
    #	clubs.php
    #	login.php
    #	news.php
    #	welcome.php

--- index.php
+++ index.php
@@ -120,6 +120,22 @@
             </div>
         </div>
         <div class="row">
+
+            <?
+            include("dbconnect.php");
+            $sql_query = "SELECT * FROM clubs";
+            $results = $db->query($sql_query);
+            while($row = $result->fetch_array())
+            {
+                $ClubName = $row['ClubName'];
+                $Description = $row['Description'];
+                echo "
+                    <h1>{$ClubName}</h1>
+                    <p>{$Description}</p>
+                    ";
+            }
+            ?>
+
             <div class="clubs">
 
             </div>
