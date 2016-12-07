39ec683ab09b15bbaf71ad95e227fb84f684a7a3
Zaki <1404039@rgu.ac.uk>
2016-12-07 18:19:31 +0000

    Merge branch 'master' of https://github.com/zaklinarycko/Coursework into Blackwidow
    # Conflicts:
    #	about.php
    #	clubs.php
    #	login.php
    #	news.php
    #	welcome.php

--- index.php
+++ index.php
@@ -127,14 +127,6 @@
                     ";
                 }
                 ?>
-                $result = $db->query($sql);
-                while($row = $result->fetch_array())
-                {
-                    $article = $row['articleID'];
-                    $articleName = $row['articleAuthor'];
-                    echo "<li><a href='blog/{$articleID}'>{$articleName}</a> by {$articleAuthor}</li>";
-                }
-                ?>
             </div>
         </div>
         <div class="row">
