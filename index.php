fc8933853909513d6d915b2a7da7117d3b63c81f
Zaki <1404039@rgu.ac.uk>
2016-12-07 19:26:04 +0000

    Merge branch 'master' of https://github.com/zaklinarycko/Coursework into Blackwidow
    # Conflicts:
    #	about.php
    #	clubs.php
    #	login.php
    #	news.php
    #	welcome.php

--- index.php
+++ index.php
@@ -121,11 +121,14 @@
         </div>
         <div class="row">
 
+
+
+            <?
+
             ini_set('display_errors', 1);
             ini_set('display_startup_errors', 1);
             error_reporting(E_ALL);
 
-            <?
             include("dbconnect.php");
             $sql_query = "SELECT * FROM clubs";
             $results = $db->query($sql_query);
