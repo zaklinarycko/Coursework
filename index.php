d51b078e2cfc7f2f8a3450e48090d164eb89f1e6
CaraH03 <1304164@rgu.ac.uk>
2016-12-09 19:41:04 +0000

    Merge branch 'master' of https://github.com/zaklinarycko/Coursework into ScarletWitch
    # Conflicts:
    #	login.php

--- index.php
+++ index.php
@@ -142,6 +142,8 @@
 
             <div class="clubs">
 
+                <iframe src="https://calendar.google.com/calendar/embed?src=n53j6l0hgeii063i8pe6k0o1f8%40group.calendar.google.com&ctz=Europe/London" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
             </div>
         </div>
     </div>
