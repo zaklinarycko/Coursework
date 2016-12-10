576cec47c48fe0817e249f12ab27593d964a0447
Zaki <1404039@rgu.ac.uk>
2016-12-10 19:02:44 +0000

    sign up try

--- index.php
+++ index.php
@@ -121,15 +121,9 @@
         </div>
         <div class="row">
 
-
-
-
-
             <div class="clubs">
             </div>
 
-
-            //calender
             <div
                 <iframe src="https://calendar.google.com/calendar/embed?src=n53j6l0hgeii063i8pe6k0o1f8%40group.calendar.google.com&ctz=Europe/London" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
             </div>
