fbb5ff9762dc5deb6b953b33b0c0a68ee086c590
Zaki <1404039@rgu.ac.uk>
2016-12-10 17:47:14 +0000

    sign up try

--- index.php
+++ index.php
@@ -269,7 +269,7 @@
             <h1>Signup Form</h1>
             <form method="post" action="signup2.php">
                 <p><input type="text" name="username" value="" placeholder="Username"></p>
-                <p><input type="text" name="email" value="" placeholder="eMail"></p>
+                <p><input type="text" name="email" value="" placeholder="email"></p>
                 <p><input type="password" name="password" value="" placeholder="Password"></p>
                 <p><input type="password" name="passwordcheck" value="" placeholder="Confirm Password"></p>
                 <p class="submit"><input type="submit" name="commit" value="Login"></p>
--- signup2.php
+++ signup2.php
@@ -10,7 +10,7 @@ $email = $_POST["email"];
 
 if ($password==$passwordcheck)
 {
-$sql = "INSERT INTO users (Username, Password, Email, AccessID) VALUES ('". $Username ."', '" .$Password."', '".$Email."', 'ID')";
+$sql = "INSERT INTO users (UserID, Username, Password, Email, DisplayName, AccessID) VALUES ('". $UserID ."', '" .$Username."', '".$Password."', '".$Email."', '".$DisplayName."', '".$AccessID."')";
 if (mysqli_query($db, $sql)) {
 } else {
 echo "Error: " . $sql . "<br>" . mysqli_error($db);
@@ -18,7 +18,7 @@ echo "Error: " . $sql . "<br>" . mysqli_error($db);
 session_start();
 $_SESSION['Username'] = $Username;
 header("location:index.php");
-$sql = "INSERT INTO users (Username, Password, Email, AccessID) VALUES ('". $Username ."', '" .$Password."', '".$Email."', 'ID')";
+$sql = "INSERT INTO users (UserID, Username, Password, Email, DisplayName, AccessID) VALUES ('". $UserID ."', '" .$Username."', '".$Password."', '".$Email."', '".$DisplayName."', '".$AccessID."')";
 }
 else
 {
