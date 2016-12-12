ca97a7bca8557310aeda175d0e31aeb449f45598
Zaki <1404039@rgu.ac.uk>
2016-12-12 16:30:02 +0000

    try

--- index.php
+++ index.php
@@ -279,37 +279,6 @@
                 <button type="submit" class="btn btn-xl" name="commit" value="Sign up"></button>
             </form>
 
-            <?
-
-            //Includes the Database connection script
-            include ("dbconnect.php");
-
-            //GETS THE USERNAME AND PASSWORD FROM PREVIOUS PAGE
-            $Username = $_POST["Username"];
-            $Password = $_POST["Password"];
-            $Passwordcheck = $_POST["Passwordcheck"];
-            $Email = $_POST["Email"];
-            $DisplayName = $_POST["DisplayName"];
-            $AccessID = $_POST["AccessID"];
-
-            if ($Password==$Passwordcheck)
-            {
-                $sql = "INSERT INTO users (UserID, Username, Password, Email, DisplayName, AccessID) VALUES ('". $UserID ."', '" .$Username."', '".$Password."', '".$Email."', '".$DisplayName."', '".$AccessID."')";
-                if (mysqli_query($db, $sql)) {
-                } else {
-                    echo "Error: " . $sql . "<br>" . mysqli_error($db);
-                }
-                session_start();
-                $_SESSION['Username'] = $Username;
-                header("location:index.php");
-                $sql = "INSERT INTO users (UserID, Username, Password, Email, DisplayName, AccessID) VALUES ('". $UserID ."', '" .$Username."', '".$Password."', '".$Email."', '".$DisplayName."', '".$AccessID."')";
-            }
-            else
-            {
-                echo "Passwords do not match";
-            }
-            ?>
-
         </div>
 
     </div>
