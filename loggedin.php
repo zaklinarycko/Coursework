3499e6a5afff8debfbd4df4552a4769512758b35
Zaki <1404039@rgu.ac.uk>
2016-12-12 12:03:50 +0000

    try

--- loggedin.php
+++ loggedin.php
@@ -19,6 +19,15 @@
 
 <body id="page-top" class="index">
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
 <!-- Navigation -->
 <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
     <div class="container">
@@ -154,7 +163,7 @@
                             </div>
                             <div class="timeline-body">
 
-                                try
+
                                 <?
 
                                 include ("dbconnect.php");
@@ -372,8 +381,8 @@
             </div>
             <div class="col-md-4">
                 <ul class="list-inline social-buttons">
+                    <div class="fb-page" data-href="https://www.facebook.com/Sportlethen/?fref=ts" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Sportlethen/?fref=ts" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Sportlethen/?fref=ts">Sportlethen CSH</a></blockquote></div>
 
-                    <li><a href="https://www.facebook.com/Sportlethen/?hc_ref=SEARCH&fref=nf"><i class="fa fa-facebook"></i></a>
                     </li>
                 </ul>
             </div>
